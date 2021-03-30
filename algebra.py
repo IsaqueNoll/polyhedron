e812f5e4c204b1b07b532288248f1e42144e0fcd
IsaqueNoll <67053121+IsaqueNoll@users.noreply.github.com>
2021-03-30 16:44:28 -0300

    Feat: add numerator and denominator methods at algebra.py

--- algebra.py
+++ algebra.py
@@ -4,3 +4,18 @@
 
 #fraction class
 class fraction():
+    def __init__(self, numerator = 1, denominator = 1):
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def setNumerator(self, value):
+        self.numerator = value
+
+    def numerator(self):
+        return self.numerator
+
+    def setDenominator(self, value):
+        self.denominator = value
+
+    def denominator(self):
+        return self.denominator
